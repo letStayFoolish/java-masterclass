4ffa3ee44bb1fe518c1ebd04a23cd546157499b4
stayFoolish <nemanjakaraklajic90@icloud.com>
2025-03-15 20:04:04 +0100

    Add methods for user input to calculate age
    Introduce `getInputFromConsole` and `getInputFromScanner` methods to handle user input and calculate age based on the provided birth year. Includes placeholder implementation for `getInputFromScanner`.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,28 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        int myAge = 35;
+//        String yourAge = "33";
+//
+//        System.out.println("My age = " + myAge);
+//        System.out.println("Our age = " + (myAge - Integer.parseInt(yourAge))); // parseInt is static method of the class Integer
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int currentYear = 2025;
+
+        System.out.println(getInputFromConsole(currentYear));
+        System.out.println(getInputFromScanner(currentYear));
+    }
+
+    public static String getInputFromConsole(int currentYear){
+        String name = System.console().readLine("Hi what is your name? ");
+        System.out.println("Hi " + name + " Thanks for taking the course!");
+
+        String dateOfBirth = System.console().readLine("What is your birth year? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return "So, you are " + age + " years old.";
+    }
+
+    public static String getInputFromScanner(int currentYear){
+        return "";
     }
 }
